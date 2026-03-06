98b19958802859d822b38e18caa82c876cdb2dad
qudi17 <eddy_github@163.com>
2026-03-06 20:23:24 +0800

    remove useless files

--- skills/github-researcher/lib/search/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types";
-export * from "./query-builder";
-export * from "./github-search-client";
-export * from "./search-service";
-export * from "./run-context";
-export * from "./selection-bridge";
